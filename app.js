f2fc7a428de69eb79a982eae3281160d8c4366e7
Cristian Romero <cristianromerovs@gmail.com>
2021-05-06 01:41:54 -0400

    primer upload

---
+++ app.js
@@ -0,0 +1,34 @@
+let num1;
+let num2;
+
+function button0(){
+    console.log("Cero");
+}
+function button1(){
+    console.log("Uno");
+}
+function button2(){
+    console.log("Dos");
+}
+function button3(){
+    console.log("Tres");
+}
+function button4(){
+    console.log("Cuatro");
+}
+function button5(){
+    console.log("Cinco");
+}
+function button6(){
+    console.log("Seis");
+}
+function button7(){
+    console.log("Siete");
+}
+function button8(){
+    console.log("Ocho");
+}
+function button9(){
+    console.log("Nueve");
+}
+
